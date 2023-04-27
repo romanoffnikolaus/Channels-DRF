c1d752a3a314c4990cc704ad2a5073fb6b4411bf
romanoffnikolaus <romanoffnikolaus@gmail.com>
2023-04-27 15:49:21 +0600

    regulars, links

--- chat/routing.py
+++ chat/routing.py
@@ -4,6 +4,5 @@ from django.urls import re_path,path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    # path('ws/chat/<str:customer>/', consumers.ChatConsumer.as_asgi())
+    re_path(r'ws/chat/(?P<room_name>\S+)/$', consumers.ChatConsumer.as_asgi())
 ]
